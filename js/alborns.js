bf7917f22edaf22b65f44a614c65fc7607f25d4d
Al Borns <al.borns@gmail.com>
2019-01-09 17:43:59 +0930

    WIP Refactored CSS into proper SCSS files Started on a title scroller, still very much a work in progess

--- js/alborns.js
+++ js/alborns.js
@@ -47,4 +47,107 @@
       $('.navbar').removeClass('d-none');
     })
   
-  })(jQuery); // End of use strict
+    /*
+        Intro headline animation
+    */
+    //set animation timing
+	var animationDelay = 2500,
+    //loading bar effect
+    barAnimationDelay = 3800,
+    barWaiting = barAnimationDelay - 3000, //3000 is the duration of the transition on the loading bar - set in the scss/css file
+    //letters effect
+    lettersDelay = 50,
+    //type effect
+    typeLettersDelay = 150,
+    selectionDuration = 500,
+    typeAnimationDelay = selectionDuration + 800,
+    //clip effect 
+    revealDuration = 600,
+    revealAnimationDelay = 1500;
+
+    initHeadline();
+
+
+    function initHeadline() {
+        //insert <i> element for each letter of a changing word
+        singleLetters($('.intro-words-wrapper').find('.word'));
+        //initialise headline animation
+        animateHeadline($('.intro-words-wrapper'));
+    }
+
+    function singleLetters($words) {
+        $words.each(function(){
+            var word = $(this),
+                letters = word.text().split(''),
+                selected = word.hasClass('is-visible');
+            for (var i in letters) {
+                if(word.parents('.rotate-2').length > 0) letters[i] = '<em>' + letters[i] + '</em>';
+                letters[i] = (selected) ? '<i class="in">' + letters[i] + '</i>': '<i>' + letters[i] + '</i>';
+            }
+            var newLetters = letters.join('');
+            word.html(newLetters).css('opacity', 1);
+        });
+    }
+
+    function animateHeadline($headlines) {
+        var duration = animationDelay;
+        $headlines.each(function(){
+            var headline = $(this);
+            
+            //trigger animation
+            setTimeout(function(){ hideWord( headline.find('.is-visible').eq(0) ) }, duration);
+        });
+    }
+
+    function hideWord($word) {
+        var nextWord = takeNext($word);
+
+            var bool = ($word.children('i').length >= nextWord.children('i').length) ? true : false;
+            hideLetter($word.find('i').eq(0), $word, bool, lettersDelay);
+            showLetter(nextWord.find('i').eq(0), nextWord, bool, lettersDelay);
+    }
+
+    function showWord($word, $duration) {
+        showLetter($word.find('i').eq(0), $word, false, $duration);
+        $word.addClass('is-visible').removeClass('is-hidden');
+    }
+
+    function hideLetter($letter, $word, $bool, $duration) {
+        $letter.removeClass('in').addClass('out');
+        
+        if(!$letter.is(':last-child')) {
+            setTimeout(function(){ hideLetter($letter.next(), $word, $bool, $duration); }, $duration);  
+        } else if($bool) { 
+            setTimeout(function(){ hideWord(takeNext($word)) }, animationDelay);
+        }
+
+        if($letter.is(':last-child') && $('html').hasClass('no-csstransitions')) {
+            var nextWord = takeNext($word);
+            switchWord($word, nextWord);
+        } 
+    }
+
+    function showLetter($letter, $word, $bool, $duration) {
+        $letter.addClass('in').removeClass('out');
+        
+        if(!$letter.is(':last-child')) { 
+            setTimeout(function(){ showLetter($letter.next(), $word, $bool, $duration); }, $duration); 
+        } else { 
+            if($word.parents('.cd-headline').hasClass('type')) { setTimeout(function(){ $word.parents('.cd-words-wrapper').addClass('waiting'); }, 200);}
+            if(!$bool) { setTimeout(function(){ hideWord($word) }, animationDelay) }
+        }
+    }
+
+    function takeNext($word) {
+        return (!$word.is(':last-child')) ? $word.next() : $word.parent().children().eq(0);
+    }
+
+    function takePrev($word) {
+        return (!$word.is(':first-child')) ? $word.prev() : $word.parent().children().last();
+    }
+
+    function switchWord($oldWord, $newWord) {
+        $oldWord.removeClass('is-visible').addClass('is-hidden');
+        $newWord.removeClass('is-hidden').addClass('is-visible');
+    }
+})(jQuery); // End of use strict
